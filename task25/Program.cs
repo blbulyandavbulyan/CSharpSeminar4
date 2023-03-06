414d41aa9a48274d612962a568f5702ba43844be
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-03-06 21:59:08 +0300

    task25, task27 were soled

---
+++ task25/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+
+Не использовать Math.Pow() и аналоги!
+*/
+double myPower(int a, uint naturalPower)
+{
+    double result = 1;
+    for(int i = 0; i < naturalPower; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральную степень: ");
+uint p =  Convert.ToUInt32(Console.ReadLine());
+Console.WriteLine(myPower(a, p));
---
+++ task27/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+int getSummOfDigits(int n)
+{
+    int result = 0;
+
+    for(int i = Math.Abs(n); i > 1; i /= 10)
+    {
+        result += (i % 10);
+    }
+    return result;
+}
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(getSummOfDigits(number));
