fd3c60be797d6aa3a2861254e2eaeca5ae4ef01e
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-03-06 20:23:05 +0300

    task24 solved

---
+++ task24/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int getInt(String msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int getSummFromTo(int start, int end)
+{
+    int result = 0;
+    for(int i = start; i <= end; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+int a = getInt("Введите чилсо: ");
+Console.WriteLine(getSummFromTo(1, a));
